7e4c2d0e18bf05f68e7a69a9af33ff6210b6c010
legamerz <111640946+legamerz@users.noreply.github.com>
2025-06-22 11:20:58 +0000

    Update intrusor.py

--- intrusor.py
+++ intrusor.py
@@ -2,24 +2,24 @@ from core.engine import run_module
 import questionary
 
 def main():
-    print("🔓 Intrusor - Pédagogie & Pentest\n")
+    print("Intrusor - Pédagogie & Pentest\n")
 
     while True:
         choix = questionary.select(
-            "📌 Que veux-tu faire ?",
+            " Que veux-tu faire ?",
             choices=[
-                "📡 Scan réseau",
-                "🍪 Audit cookies sensibles",
-                "❌ Quitter"
+                " Scan réseau",
+                "Audit cookies sensibles",
+                "Quitter"
             ]
         ).ask()
 
-        if choix == "📡 Scan réseau":
+        if choix == "Scan réseau":
             run_module("scan")
-        elif choix == "🍪 Audit cookies sensibles":
+        elif choix == "Audit cookies sensibles":
             run_module("cookie_audit")
-        elif choix == "❌ Quitter":
-            print("👋 À bientôt sur Intrusor !")
+        elif choix == "Quitter":
+            print("À bientôt sur Intrusor !")
             break
 
 if __name__ == "__main__":
